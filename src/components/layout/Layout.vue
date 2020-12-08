23e979526cc9376d006af9d9bfec998a2c2dc764
io24m <io24m@outlook.com>
2020-12-08 11:22:37 +0800

    i

--- src/components/layout/Layout.vue
+++ src/components/layout/Layout.vue
@@ -1,13 +1,12 @@
 <template>
   <q-layout view="hHh LpR fFf">
-
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="left = !left" />
 
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg">
+            <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" />
           </q-avatar>
           Title
         </q-toolbar-title>
@@ -15,22 +14,24 @@
     </q-header>
 
     <q-drawer show-if-above v-model="left" side="left" bordered>
+      <SideBar />
       <!-- drawer content -->
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script>
+import SideBar from "./SideBar";
 export default {
-  data () {
+  components: { SideBar },
+  data() {
     return {
-      left: false
-    }
-  }
-}
+      left: false,
+    };
+  },
+};
 </script>
---
+++ src/components/layout/SideBarItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <template v-if="item.children">
+      <q-expansion-item
+        icon="mail"
+        :header-inset-level="headerInsetLevel"
+        :label="item.name"
+      >
+        <SideBarItem
+          v-for="(c, index) in item.children"
+          :key="index"
+          :item="c"
+          :header-inset-level="headerInsetLevel + 0.5"
+        />
+      </q-expansion-item>
+    </template>
+    <template v-else>
+      <q-item to="/inbox" exact :inset-level="headerInsetLevel">
+        <q-item-section avatar>
+          <q-icon name="inbox" />
+        </q-item-section>
+        <q-item-section> {{ item.name }} </q-item-section>
+      </q-item>
+    </template>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    item: {},
+    headerInsetLevel: {
+      default: 0,
+    },
+  },
+  name: "SideBarItem",
+};
+</script>
